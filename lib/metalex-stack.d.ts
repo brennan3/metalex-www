912c1411801f8efaea87daf155529153ecaea3a3
Mike Brennan <mike@delphidigital.io>
2023-08-18 09:20:29 -0400

    latest build

--- lib/metalex-stack.d.ts
+++ lib/metalex-stack.d.ts
@@ -2,11 +2,12 @@ import { aws_cloudfront as cloudfront, aws_s3 as s3, aws_certificatemanager, Cfn
 import { Construct } from 'constructs';
 export interface MetalexStackProps extends StackProps {
     domain: string;
+    subjectAlternativeNames: string[];
 }
 export declare class MetalexStack extends Stack {
     readonly distributionDomainName: CfnOutput;
-    constructor(scope: Construct, id: string, { domain, ...props }: MetalexStackProps);
+    constructor(scope: Construct, id: string, { domain, subjectAlternativeNames, ...props }: MetalexStackProps);
     setupWebsiteBucket(): s3.Bucket;
-    setupCertification(domainName: string): aws_certificatemanager.Certificate;
+    setupCertification(domainName: string, subjectAlternativeNames: string[]): aws_certificatemanager.Certificate;
     setupWebsiteS3StaticFileDeploy(destinationBucket: s3.Bucket, distribution: cloudfront.Distribution): void;
 }
--- lib/metalex-stage.d.ts
+++ lib/metalex-stage.d.ts
@@ -2,8 +2,9 @@ import { CfnOutput, Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 export interface MetalexStageProps extends StageProps {
     domain: string;
+    subjectAlternativeNames: string[];
 }
 export declare class MetalexStage extends Stage {
     readonly distributionDomainName: CfnOutput;
-    constructor(scope: Construct, id: string, { domain, ...props }: MetalexStageProps);
+    constructor(scope: Construct, id: string, { domain, subjectAlternativeNames, ...props }: MetalexStageProps);
 }
